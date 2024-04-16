646c1f1c698741394a8babf4636c618b2c4aa77c
Mariano Villalonga <78375048+marianovillalonga@users.noreply.github.com>
2024-04-16 16:40:22 -0300

    Add files via upload

--- MarianoVillalonga/laboratorio3/js/helper.js
+++ MarianoVillalonga/laboratorio3/js/helper.js
@@ -31,3 +31,25 @@ function calcularArea() {
     }
 }
 
+function compararCadenas() {
+    var cadena1 = document.getElementById("cadena1").value;
+    var cadena2 = document.getElementById("cadena2").value;
+    
+    var mayor = cadena1.localeCompare(cadena2);
+    var resultado = "";
+    if (mayor === 0) {
+        resultado += "Las cadenas son iguales en orden alfabético.";
+    } else if (mayor > 0) {
+        resultado += "La mayor es: " + cadena1 + ".";
+    } else {
+        resultado += "La mayor es: " + cadena2 + ".";
+    }
+    
+    var concatenadas = cadena1 + " " + cadena2;
+    
+    var masLarga = (cadena1.length > cadena2.length) ? cadena1 : cadena2;
+    
+    document.getElementById("resultado1").textContent = "LA MAYOR ES: " + resultado;
+    document.getElementById("resultado2").textContent = "LAS CADENAS CONCATENADAS QUEDAN: " + concatenadas;
+    document.getElementById("resultado3").textContent = "LA CADENA MÁS LARGA ES: " + masLarga + " Y TIENE " + masLarga.length + " CARACTERES";
+}
